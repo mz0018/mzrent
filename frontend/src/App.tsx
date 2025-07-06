2c5af98fa02e3efdab575a909010f51eb1e8312a
mz0018 <martinezhanzmenzi@gmail.com>
2025-07-06 11:50:51 +0700

    Tailwind installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <div>Welcome</div>
+      <div className='p-4 bg-red-500 text-white rounded-lg shadow-lg w-1/2 hover:bg-pink-800 transition-all'>Welcome</div>
     </>
   )
 }
