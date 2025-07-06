8ce1ac4b4fd5ddd41fc555f84a62bd19b087ac06
mz0018 <martinezhanzmenzi@gmail.com>
2025-07-06 09:56:32 +0700

    First Initial Commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
